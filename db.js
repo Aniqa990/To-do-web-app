f4e925ed0f1573a7ea3b58028d0c155769afed41
Aniqa990 <117656214+Aniqa990@users.noreply.github.com>
2024-06-07 12:50:21 +0500

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "brownie4u",
+    password: "pass",
     database: "todo_database",
     host: "localhost",
     port: 5432
